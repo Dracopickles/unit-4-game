adaebc66f23a28cb34c28d8f123c07b9358c6cce
Karl Lockbaum <jrlockbaum@sbcglobal.net>
2019-08-17 07:23:07 -0700

    initial site files

---
+++ assets/javascript/game.js
@@ -0,0 +1,26 @@
+// making variables for crystals, 
+
+var crystalOne = Math.floor(Math.random() * 12)+1;
+
+var gameNumber = Math.floor(Math.random() * 102)+19;
+
+var userScore = 0;
+
+var wins = 0;
+
+var loses = 0;
+
+// create a function that displays all your stats
+
+// create a function that resets game in the reset funtion you want to 
+//set the total score back to zero and generate new random numbers. 
+// and have it call first function for reset 
+
+// create a function to check for the win and the first part is to 
+//have a if statement to check if there number is equal to the gamenumber
+//if so add to win and if they win call the reset function at the end
+//and a else if, if the user score is greater then the number they lose
+//increase there loses and call the reset the function
+
+//create a document ready function at the bottom
+//create on.clicks for crystals to be tied to there variables and reset function
